772fdeca4de0859a6012d52f82930fe226c1c9e6
not-the <87151784+not-the@users.noreply.github.com>
2024-10-12 17:57:12 -0400

    Fireworks SFX, moved ui to top right

--- js/sound.mjs
+++ js/sound.mjs
@@ -3,40 +3,44 @@ import { sound as PIXISound } from '../lib/pixi-sound.mjs'
 
 /** Sound effects list */
 const sounds = {
-    "thunder1": {
-        src: "./assets/sfx/thunder1.mp3",
-        volume: 0.5
-    },
-    "thunder2": {
-        src: "./assets/sfx/thunder2.mp3",
-        volume: 0.5
-    },
-    "thunder3": {
-        src: "./assets/sfx/thunder3.mp3",
-        volume: 0.5
-    },
+    // Thunder
+    "thunder1": { src: "./assets/sfx/thunder1.mp3" },
+    "thunder2": { src: "./assets/sfx/thunder2.mp3" },
+    "thunder3": { src: "./assets/sfx/thunder3.mp3" },
+
+    // Explosions
     "explosion1": {
         src: "./assets/sfx/explosion1.mp3",
-        volume: 0.2
+        volume: 0.4
     },
+
+    // Fire
     // "fire": {
     //     src: "./assets/sfx/fire.mp3",
     //     volume: 0.7,
     //     loop: true,
     //     type: "fade"
     // },
+
+    // Fireworks
+    "fireworks1": { src: "./assets/sfx/firework_explosion_001.mp3" },
+    "fireworks2": { src: "./assets/sfx/firework_explosion_002.mp3" },
+    // "fireworks3": { src: "./assets/sfx/firework_explosion_fizz_001.mp3" },
+    "fireworks4": { src: "./assets/sfx/firework_explosion_fizz_002.mp3" },
 }
 // Register sounds
 for(const [key, {src}] of Object.entries(sounds)) PIXISound.add(key, src);
 
 /** Audio methods */
 const sound = {
+    volume_master: 0.5,
+
     recents: {},
     play(name, volume) {
         if(this.recents[name] > Date.now()-100) return; // Already played sound within last 100ms
 
-        let options = {
-            volume: volume ?? sounds[name].volume,
+        const options = {
+            volume: (volume ?? sounds?.[name]?.volume ?? 1) * this.volume_master,
             loop: sounds[name].loop,
             complete: () => console.log(name + ' complete')
         }
